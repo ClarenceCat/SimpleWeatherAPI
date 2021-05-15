2bbbfaf6de8197d5973135d3486b838d1e324960
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-05-14 20:22:49 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,5 @@
+// file: server.js
+// description: this file initializes the database used in the application, and starts the server.
+// programmer: Daniel Grew
+// Date Last Modified: 2021-05-14
+
