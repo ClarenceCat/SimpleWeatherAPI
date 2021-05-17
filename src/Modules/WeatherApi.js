f000e8efa5767a26da04fca4878b901757526a70
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-05-16 20:24:14 -0400

    finished logic for the getWeather function

--- src/Modules/WeatherApi.js
+++ src/Modules/WeatherApi.js
@@ -5,6 +5,8 @@
 
 // require https to call the api
 const https = require('https');
+const axios = require('axios');
+const logger = require('../Middleware/logger');
 
 // function: getWeather
 // description: This function calls the OpenWeather API to retrieve the current weather information for a given city
@@ -17,9 +19,47 @@ const https = require('https');
 //          WeatherInfo : { "temp" : float, "feels_like" : float, "temp_min" : float, "temp_max" : float, "pressure" : Number, "humidity" : Number }
 const getWeather = async (cityName) => {
     // create the api call string
-    const api_call = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHER_API_KEY}`;
+    const api_call_str = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHER_API_KEY}`;
 
     // note - OpenWeather API returns status code 404 with message "city not found" when the city does not exist or cannot be found (error checking)
     // enter try catch block
+    try{
+        // log the call to open weather api
+        logger.info(`Making GET request to the OpenWeather API (@openweathermap.org) with the city name ${city}`);
+
+        // make get request to the openweather api
+        const api_data = await axios.get(api_call_str);
+
+        logger.info(`Open Weather API responded with a status code of ${api_data.status}`);
+
+        // extract the data from the response object
+        const weather_info = api_data.data.main;
+
+        // there is an error accessing the info from the data retrieved from the api
+        if(!weather_info) {
+            logger.warn(`Unable to extract weather info from the OpenWeather api when requesting info for the city ${cityName}`);
+            return null;
+        }
+
+        return weather_info;
+    }
+    catch(error)
+    {
+        // an error will be thrown if there is an axios error, or if the api responds with a status code outside of the 200-299 range
+        // if this was a status code that is outside of the above specified range (2xx) - then this will be triggered
+        if(error.response){
+            logger.info(`Open Weather API responded with a status code of ${api_data.status} - ${ error.response.data }`);
+            
+            // return null
+            return null;
+        }
+        else{
+            // if this code is called, then it is an issue with axios 
+            logger.error(`Error making request to the Open Weather Api from this application`);
+        }
+
+    }
 
 }
+
+module.exports = { getWeather };
