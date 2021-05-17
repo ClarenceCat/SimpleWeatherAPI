4b3c6a8bfe13cb0014201eee696ca305d9361ed3
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-05-16 20:02:56 -0400

    Added axios to dependencies. Also added logic and comments to WeatherAPI.js

--- src/Modules/WeatherApi.js
+++ src/Modules/WeatherApi.js
@@ -3,4 +3,23 @@
 // Programmer: Daniel Grew
 // Date Last Modified: 2021-05-16
 
+// require https to call the api
+const https = require('https');
 
+// function: getWeather
+// description: This function calls the OpenWeather API to retrieve the current weather information for a given city
+// parameters:
+//      cityName : String - the name of the city to retrieve the weather for
+// returns:
+//      WeatherInfo : Object - this is null if the API returns an error or the city does not exist OR returns an object
+//          containing the weather information for the specified city
+//          
+//          WeatherInfo : { "temp" : float, "feels_like" : float, "temp_min" : float, "temp_max" : float, "pressure" : Number, "humidity" : Number }
+const getWeather = async (cityName) => {
+    // create the api call string
+    const api_call = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHER_API_KEY}`;
+
+    // note - OpenWeather API returns status code 404 with message "city not found" when the city does not exist or cannot be found (error checking)
+    // enter try catch block
+
+}
---
+++ src/config/constants.js
@@ -0,0 +1,2 @@
+// file: constants.js
+// description: This file contains the constants used throughout the application
